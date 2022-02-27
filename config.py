001ccb9939d51a4b84258fbc338f41a93e82fd61
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-28 02:35:04 +0300

    Adding the Production configuration child class

--- config.py
+++ config.py
@@ -7,3 +7,13 @@ class Config:
 
    
     pass
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
