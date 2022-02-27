b2b1c4bbe0d45ec50f5227ba275d61f1c464a18e
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-28 02:47:44 +0300

    Creating a function that gets response for url

--- app/request.py
+++ app/request.py
@@ -7,3 +7,23 @@ def configure_request(app):
     source_url = app.config['NEWS_SOURCES_BASE_URL']
     articles_url = app.config['ARTICLES_BASE_URL']
     api_key = app.config['NEWS_API_KEY']
+
+
+def get_sources(category):
+    '''
+    Function to get json response for our url request
+    '''
+    get_sources_url = source_url.format(category,api_key)
+    print(get_sources_url)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources(sources_results_list)
+
+    return sources_results
