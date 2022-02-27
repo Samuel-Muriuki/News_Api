be7be789e123d44d6c4ea9a1659ffe695af8ba77
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-28 02:26:42 +0300

    Creating the appconfigurations

--- app/models.py
+++ app/models.py
@@ -5,4 +5,9 @@ from config import config_options
 bootstrap = Bootstrap()
 
 
+def create_app(config_name):
+    
+    app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
