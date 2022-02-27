8d0cb4202b43c20767a8b1239f394add1ca4ee85
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-28 02:33:54 +0300

    Adding the Config class

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    NEWS_API_KEY ='53ffdeb94a4d451e9f763453ed4c7665'
+
+   
+    pass
